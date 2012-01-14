eae993372136a0489679cc8c9d96580401128221
kdcl <kdcl03@gmail.com>
2012-01-14 19:08:01 +0800

    Initial Commit

---
+++ VowelCounter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  VowelCounter
+//
+//  Created by  on 12/1/14.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
